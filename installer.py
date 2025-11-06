0c17819aa888c3b808ac16090de30ce42adbe5e3
hegehog8761 <119626198+hegehog8761@users.noreply.github.com>
2025-11-06 12:03:02 +0000

    Continued installer creation (non-functional)

--- installer.py
+++ installer.py
@@ -1,4 +1,12 @@
 import os, system
 
-if system.system() == "Linux" or "Linux" in system.system():
-  
+if platform.platform() == "Linux" or "Linux" in platform.platform():
+    if os.getuid() != 0:
+        # Request admin as not already ran
+        os.execvp('sudo', ['sudo', sys.executable] + sys.argv)
+      os.system("sudo apt install hostnamectl -y")
+elif platform.platform() == "Windows" or "Windows" in platform.platform():
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    # Check if bonjour downloaded, if not install it
+    
