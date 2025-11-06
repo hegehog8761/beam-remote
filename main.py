0fd8576540c11e9d9634365872fa0d93147f2b20
hegehog8761 <119626198+hegehog8761@users.noreply.github.com>
2025-11-06 11:58:05 +0000

    Removed old local address registration

--- main.py
+++ main.py
@@ -16,25 +16,6 @@ import server, address_register
 
 original_hostname = "somedevice"
 
-"""
-if os.name == "posix":
-    if os.getuid() != 0:
-        # Request admin as not already ran
-        print("This application requires admin privilidges")
-        os.execvp('sudo', ['sudo', sys.executable] + sys.argv)
-    # Set up beamremote.local as ip address
-    base_hosts = open("/etc/hosts").read()
-    if "127.0.0.1 beamremote.local" not in base_hosts:
-        open("/etc/hosts", "a").write("\n127.0.0.1 beamremote.local\n")
-    else:
-        print("The address used for the remote is already registered to this device, it will automatically be de-registered when this script stops")
-elif os.name == "nt":
-    print("Windows setup is not complete yet, exiting...")
-    sys.exit(1)
-
-""" # Removed as couldn't get beamremote.local to work for now
-
-
 if platform.platform() == "Linux" or "Linux" in platform.platform():
     if os.getuid() != 0:
         # Request admin as not already ran
