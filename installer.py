e6297eed129472986229891b7f814a06ad823c8d
hegehog8761 <119626198+hegehog8761@users.noreply.github.com>
2025-11-06 13:36:45 +0000

    Created Venv

--- installer.py
+++ installer.py
@@ -1,10 +1,10 @@
-import os, system
+import os, platform, ctypes, sys
 
 if platform.platform() == "Linux" or "Linux" in platform.platform():
     if os.getuid() != 0:
         # Request admin as not already ran
         os.execvp('sudo', ['sudo', sys.executable] + sys.argv)
-      os.system("sudo apt install hostnamectl -y")
+    os.system("sudo apt install hostnamectl avahi-daemon -y")
 elif platform.platform() == "Windows" or "Windows" in platform.platform():
     if not ctypes.windll.shell32.IsUserAnAdmin():
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
